ca947a8ea1fdeba18879d731e712a38340cf9bc8
nagysoma03 <nagyzszsomazsolt@uni.sapientia.ro>
2024-02-24 13:53:03 +0200

    Miniprojekt1

--- Miniprojekt1/src/fel6.java
+++ Miniprojekt1/src/fel6.java
@@ -1,19 +1,22 @@
-import java.util.Arrays;
-
 public class fel6 {
-    static void tombOsszehasonlitas(int[] array1, int[] array2) {
-        if (Arrays.equals(array1, array2)) {
-            System.out.println("A ket tomb egyenlo!");
-        } else {
-            System.out.println("A tombok nem egyenloek!");
+    static boolean tombOsszehasonlitas(int[] array1, int[] array2) {
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] != array2[i]) {
+                return false;
+            }
         }
+        return true;
     }
 
     public static void main(String[] args) {
         int[] iArray1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] iArray2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] iArray3 = {0, 1, 3, 5, 6, 11, 55};
-        tombOsszehasonlitas(iArray1, iArray2);
-        tombOsszehasonlitas(iArray1, iArray3);
+        if (tombOsszehasonlitas(iArray1, iArray2)) {
+            System.out.println("A ket tomb egyforma.");
+        }
+        if (!tombOsszehasonlitas(iArray1, iArray3)) {
+            System.out.println("A tombok nem egyformak.");
+        }
     }
 }
