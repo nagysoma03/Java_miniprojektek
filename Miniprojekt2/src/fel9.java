5dd3092968828322bc5da7758873ee3fd0bb3721
nagysoma03 <nagyzszsomazsolt@uni.sapientia.ro>
2024-03-10 16:07:16 +0200

    Miniprojekt2

--- Miniprojekt2/src/fel9.java
+++ Miniprojekt2/src/fel9.java
@@ -1,22 +1,21 @@
 import java.util.Arrays;
 
 public class fel9 {
-    public static void main(String[] args) {
-        int[][] array2D = new int[10][10];
-
-        array2D[1][1] = 1;
-        array2D[2][2] = 2;
-        array2D[3][3] = 3;
-        array2D[4][4] = 4;
-        array2D[5][5] = 5;
-        array2D[6][6] = 6;
-        array2D[7][7] = 7;
-        array2D[8][8] = 8;
-        array2D[9][9] = 9;
-
-        for (int[] rows : array2D) {
+    public static void twoDimensionalArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (i==j) {
+                    array[i][j] = i;
+                }
+            }
+        }
+        for (int[] rows : array) {
             System.out.println(Arrays.toString(rows));
         }
+    }
 
+    public static void main(String[] args) {
+        int[][] array2D = new int[10][10];
+        twoDimensionalArray(array2D);
     }
 }
